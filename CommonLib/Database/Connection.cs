31d0d6429bd12930a482ce8d0ce47391c5c8a842
Shintaro Hirasawa <nobizo@gmail.com>
2017-03-03 10:14:10 +0900

    データベース接続クラス追加 設定ファイル追加

---
+++ CommonLib/Database/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace CommonLib.Database
+{
+    /// <summary>
+    /// Singletonのデータベース接続クラス
+    /// </summary>
+    public class Connection
+    {
+        private static Connection Instance = null;
+        private static MySqlConnection conn = null;
+        private static MySqlTransaction tran = null;
+
+        #region コンストラクタ
+        private Connection()
+        {
+            var bl = new MySqlConnectionStringBuilder();
+            bl.ConnectionProtocol = MySqlConnectionProtocol.Tcp;
+        }
+        #endregion
+
+        #region メソッド(public)
+        public static Connection GetInstance()
+        {
+            return Instance;
+        }
+        #endregion
+    }
+}
