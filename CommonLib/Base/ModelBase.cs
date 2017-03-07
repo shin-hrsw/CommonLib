928cacc607bd173e0d81f3a6b83a37b69d513dd1
Shintaro Hirasawa <nobizo@gmail.com>
2017-03-07 09:55:35 +0900

    Insert処理を追加

--- CommonLib/Base/ModelBase.cs
+++ CommonLib/Base/ModelBase.cs
@@ -14,6 +14,16 @@ namespace CommonLib.Base
     /// </remarks>
     abstract class ModelBase
     {
+        // 制御で使用するプロパティのリスト
+        // "NotTableColumnAttribute"のような属性を作って制御してもいいが…
+        private List<string> control_properties = new List<string>()
+        { "TableName", "KeyInfomation", "UseAutoIncrement" };
+
+        private bool is_new;        // 新規データの場合にtrue
+        private bool is_changed;    // 変更された場合にtrue
+        private bool is_deleted;    // 削除された場合にtrue
+        private string error;       // エラーが発生した場合に設定
+
         #region プロパティ(abstract)
         /// <summary>
         /// テーブル名
@@ -40,6 +50,79 @@ namespace CommonLib.Base
         #region メソッド(public)
         public bool Update()
         {
+            if (this.is_new)
+            {
+                return true;
+            }
+
+            if (this.is_changed)
+            {
+                return true;
+            }
+
+            if (this.is_deleted)
+            {
+                return true;
+            }
+
+            // SQL発行の必要がない
+            return true;
+        }
+
+        public string GetError()
+        {
+            return this.error;
+        }
+        #endregion
+
+        #region メソッド(private)
+        private bool Insert()
+        {
+            Type ty = this.GetType();
+            // プロパティから列リストを作る
+            // 制御用プロパティは除外
+            // AUTO_INCREMENTを使用する場合主キーは除外
+            var props = ty.GetProperties()
+                .Where(x =>
+                {
+                    if (this.control_properties.Contains(x.Name)) { return false; }
+                    if (this.UseAutoIncrement)
+                    {
+                        if (this.KeyInfomation.ContainsKey(x.Name)) { return false; }
+                        return true;
+                    }
+                    else { return true; }
+                });
+            string columns = "";
+            string par = "";
+            var param_list = new List<Database.SQLParameter>();
+            foreach (var p in props)
+            {
+                columns += p.Name + ", ";
+                par = "@" + p.Name + ", ";
+                param_list.Add(new Database.SQLParameter(
+                    "@" + p.Name, p.GetType(), p.GetValue(this)));
+            }
+            columns = columns.Substring(0, columns.Length - 2);
+            par = par.Substring(0, par.Length - 2);
+
+            var sql = new StringBuilder("INSERT INTO ");
+            sql.Append(this.TableName);
+            sql.Append("(");
+            sql.Append(columns);
+            sql.Append(") VALUES(");
+            sql.Append(par);
+            sql.Append(")");
+
+            Database.Connection.ExecuteNonQuery(sql.ToString(), param_list);
+            // AUTO_INCREMENT使用時は設定されたIDを主キーにセット
+            if (this.UseAutoIncrement)
+            {
+                var id = int.Parse(Database.Connection.ExecuteScalar("SELECT last_insert_id()").ToString());
+                var p = ty.GetProperty(this.KeyInfomation[0].Name);
+                p.SetValue(this, id);
+            }
+
             return true;
         }
         #endregion
